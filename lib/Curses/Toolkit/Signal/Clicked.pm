ecb71397562fd1d7ee9aff58eb921bf2ffb7356c
Damien Krotkine <dams@cpan.org>
2009-11-02 10:24:08 +0100

    cleanup

--- lib/Curses/Toolkit/Signal/Clicked.pm
+++ lib/Curses/Toolkit/Signal/Clicked.pm
@@ -15,7 +15,9 @@ Curses::Toolkit::Signal::Clicked
 
 Signal triggered when a widget is clicked or activated
 
-=head1 CONSTRUCTOR
+=head1 SYNOPSIS
+
+  $widget->signal_connect( clicked => sub { do_stuf(...) } );
 
 =cut
 
@@ -50,95 +52,6 @@ sub generate_listener {
 			$code_ref->();
 		},
 	);
-}
-
-=head2 new
-
-  input : type   : a type of mouse click. STRING, can be one of (see below)
-          button : the button that was clicked. STRING, can be (see below) 
-          coordinates : Curses::Toolkit::Object::Coordinates : where the click happened
-          root_window : Curses::Toolkit : the root window object
-  
-  type can be one of :
-  pressed released clicked double_clicked
-  triple_clicked shift ctrl alt
-
-  button can be one of :
-  button1 button2 button3 button4 button5 button
-
-=cut
-
-sub new {
-	my $class = shift;
-	my $self = $class->SUPER::new();
-	my %args = validate( @_,
-						 { type => 
-						   { type => SCALAR,
-							 callbacks => { 'must be one of ' . join(', ', $self->get_possible_types()) =>
-											sub { my %h = map { $_ => 1 } $self->get_possible_types(); $h{ $_[0] }; },
-										  },
-						   },
-						   button =>
-						   { type => SCALAR,
-							 callbacks => { 'must be one of ' . join(', ', $self->get_possible_buttons()) =>
-											sub { my %h = map { $_ => 1 } $self->get_possible_buttons(); $h{ $_[0] }; },
-										  },
-						   },
-						   coordinates => { isa => 'Curses::Toolkit::Object::Coordinates' },
-						   root_window => { isa => 'Curses::Toolkit' },
-						 }
-					   );
-	$self = bless(\%args, $class);
-	return $self;
-}
-
-sub get_possible_types {
-	my ($self) = @_;
-	return qw(pressed released clicked double_clicked triple_clicked shift ctrl alt);
-}
-
-sub get_possible_buttons {
-	my ($self) = @_;
-	return qw(button1 button2 button3 button4 button5 button);
-}
-
-=head2 get_matching_widget
-
-Returns the widget that is affected by the event. In this case, it returns the
-widget where the click were done
-
-  input  : none
-  output : the widget that is affected by the event
-
-=cut
-
-sub get_matching_widget {
-	my ($self) = @_;
-
-	my $recurse;
-	$recurse = sub { 
-		my $deepness = shift;
-		my $stack = shift;
-		$deepness++;
-		my @result = map { 
-			my $stack = ( $_->isa('Curses::Toolkit::Widget::Window') ? 
-						  $_->get_property(window => 'stack') : $stack
-						);
-			[ $deepness,
-			  $stack,
-			  $_
-			], ( $_->can('get_children') ? $recurse->($deepness, $stack, $_->get_children()) : () )	  
-		} @_;
-		return @result;
-	};
-
-	my @all_widgets = $recurse->(0, 0, $self->{root_window}->get_windows());
-	# sort by window stack then deepnes in the widget tree
-	@all_widgets = sort { $b->[1] <=> $a->[1] || $b->[0] <=> $a->[0] } grep { $self->{coordinates}->is_in_widget($_->[2]) } @all_widgets;
-
-
-	@all_widgets and return $all_widgets[0]->[2];
-	return $self->{root_window};
 }
 
 1;
