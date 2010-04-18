50bc9dbef3060064e073bc23bff5e8caa34b1670
Damien Krotkine <dams@cpan.org>
2010-04-18 17:05:02 +0200

    added scroll area

---
+++ examples/widgets/scroll_area.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use FindBin qw( $Bin );
+use lib "$Bin/../../lib";
+main() unless caller;
+
+sub main {
+
+	use POE::Component::Curses;
+
+	use Curses::Toolkit::Widget::Window;
+	use Curses::Toolkit::Widget::VBox;
+	use Curses::Toolkit::Widget::HBox;
+	use Curses::Toolkit::Widget::Button;
+	use Curses::Toolkit::Widget::Border;
+	use Curses::Toolkit::Widget::Entry;
+	use Curses::Toolkit::Widget::HPaned;
+	use Curses::Toolkit::Widget::VPaned;
+	use Curses::Toolkit::Widget::Label;
+
+	use Curses::Toolkit::Widget::ScrollArea;
+
+	my $root = POE::Component::Curses->spawn();
+
+    my $window = Curses::Toolkit::Widget::Window
+                   ->new()
+                   ->set_name('window2')
+                   ->set_title("testing scroll area");
+
+	$root->add_window( $window );
+
+    $window->add_widget(
+      Curses::Toolkit::Widget::ScrollArea->new
+        ->add_widget(
+          Curses::Toolkit::Widget::HBox->new
+            ->pack_end(
+              Curses::Toolkit::Widget::Border->new
+                ->add_widget(
+                  Curses::Toolkit::Widget::Label->new()
+                    ->set_text("This is a quite long label. Actually, it is <b>very</b> long. How long can it be ? Not sure...")
+                )
+            )
+            ->pack_end(
+              Curses::Toolkit::Widget::Border->new
+                ->add_widget(
+                  Curses::Toolkit::Widget::Label->new()
+                    ->set_text("THIS IS A QUITE LONG LABEL. ACTUALLY, IT IS <B>VERY</B> LONG. HOW LONG CAN IT BE ? NOT SURE...")
+                )
+            ),
+        )
+    );
+
+	$window->set_coordinates(
+		x1 => '0',   y1 => '0',
+		x2 => '100%', y2 => '100%',
+	);
+
+	POE::Kernel->run();
+}
---
+++ lib/Curses/Toolkit/Widget/ScrollArea.pm
@@ -0,0 +1,54 @@
+use warnings;
+use strict;
+
+package Curses::Toolkit::Widget::ScrollArea;
+
+# ABSTRACT: a vertical scrollable area
+
+use parent qw(Curses::Toolkit::Widget::Bin);
+
+use Params::Validate qw(:all);
+
+sub new {
+    my $class = shift;
+    my $self  = $class->SUPER::new();
+#    $self->{visibility_mode} = 'auto';
+    return $self;
+}
+
+# Returns the relative rectangle that a child widget can occupy. Overloads the
+# method from Curses::Toolkit::Widget::Container : we return the virtual space
+# of the scroll area, instead of the real coordinates.
+#
+# input  : none
+# output : a Curses::Toolkit::Object::Coordinates object
+
+sub _get_available_space {
+    my ($self) = @_;
+#    my $rc = $self->get_relatives_coordinates();
+    use Curses::Toolkit::Object::Coordinates;
+    return Curses::Toolkit::Object::Coordinates->new(
+        x1 => 0,   y1 => 0,
+        x2 => 20, y2 => 20,
+    );
+}
+
+=head2 get_desired_space
+
+Given a coordinate representing the available space, returns the space desired
+The ScrollArea desires all the space available, so it returns the available space
+
+  input : a Curses::Toolkit::Object::Coordinates object
+  output : a Curses::Toolkit::Object::Coordinates object
+
+=cut
+
+sub get_desired_space {
+
+    my ( $self, $available_space ) = @_;
+
+    my $desired_space = $available_space->clone();
+    return $desired_space;
+}
+
+1;
