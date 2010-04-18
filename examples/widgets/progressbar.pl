58151bb622093dbc8dd9f9ef582107bd93c4064d
Damien Krotkine <dams@cpan.org>
2010-04-18 17:01:44 +0200

    add vprogressbar in test

--- examples/widgets/progressbar.pl
+++ examples/widgets/progressbar.pl
@@ -17,14 +17,16 @@ sub main {
     use Curses::Toolkit::Widget::Border;
     use Curses::Toolkit::Widget::Button;
     use Curses::Toolkit::Widget::HProgressBar;
+    use Curses::Toolkit::Widget::VProgressBar;
 
     my $root = POE::Component::Curses->spawn;
 
-    my $bar;
+    my $hbar;
+    my $vbar;
     {
         my $window1 =
           Curses::Toolkit::Widget::Window->new->set_name('window')->set_title("manual progress bar")
-              ->set_coordinates( x1 => 0, y1 => 0, x2 => '100%', y2 => 7 );
+              ->set_coordinates( x1 => 0, y1 => 0, x2 => '100%', y2 => 30 );
         $root->add_window($window1);
 
         $window1->add_widget(
@@ -42,7 +44,8 @@ sub main {
                 ->pack_end(
                   Curses::Toolkit::Widget::Button->new_with_label('-')->signal_connect( clicked => 
                       sub {
-                          $bar->set_position( $bar->get_position - 1 );
+                          $hbar->set_position( $hbar->get_position - 1 );
+                          $vbar->set_position( $hbar->get_position - 1 );
                       }),
                   { expand => 0 },
                 )
@@ -50,7 +53,15 @@ sub main {
               { expand => 0 },
             )
             ->pack_end(
-              $bar  = Curses::Toolkit::Widget::HProgressBar->new,
+              Curses::Toolkit::Widget::VBox->new
+              ->pack_end(
+                $hbar  = Curses::Toolkit::Widget::HProgressBar->new,
+                { expand => 1 },
+              )
+               ->pack_end(
+                 $vbar  = Curses::Toolkit::Widget::VProgressBar->new,
+                 { expand => 1 },
+               ),
               { expand => 1 },
             )
             ->pack_end(
@@ -64,7 +75,8 @@ sub main {
                 ->pack_end(
                   Curses::Toolkit::Widget::Button->new_with_label('+')->signal_connect( clicked =>
                       sub {
-                          $bar->set_position( $bar->get_position + 1 );
+                          $hbar->set_position( $hbar->get_position + 1 );
+                          $vbar->set_position( $hbar->get_position + 1 );
                       }),
                   { expand => 0 },
                 )
