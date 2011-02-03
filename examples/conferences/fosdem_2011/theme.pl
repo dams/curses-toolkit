7ab85ad6be74e9649fd754b39e8a8944caef87eb
Damien Krotkine <dams@cpan.org>
2011-02-03 17:14:33 +0100

    theme example

--- examples/conferences/fosdem_2011/theme.pl
+++ examples/conferences/fosdem_2011/theme.pl
@@ -6,32 +6,44 @@ use warnings;
 use FindBin qw( $Bin );
 use lib "$Bin/../../../lib";
 
+use relative -to      => "Curses::Toolkit::Widget",
+             -aliased => qw(Window Label VBox HBox Button);
+use relative -to      => "Curses::Toolkit::Theme::Default::Color",
+             -aliased => qw(BlueWhite Yellow Pink);
+use relative -to      => "Curses::Toolkit::Theme",
+             -aliased => qw(Default);
+
 main() unless caller;
 
 sub main {
     use POE::Component::Curses;
 
-    use Curses::Toolkit::Widget::Window;
-    use Curses::Toolkit::Widget::Label;
-    use Curses::Toolkit::Widget::VBox;
-    use Curses::Toolkit::Widget::HBox;
-    use Curses::Toolkit::Widget::Button;
+    my $root = POE::Component::Curses->spawn( args => { theme_name => BlueWhite } );
 
-    use Curses::Toolkit::Theme::Default::Color::BlueWhite;
-    my $root = POE::Component::Curses->spawn( args => { theme_name => 'Curses::Toolkit::Theme::Default::Color::BlueWhite' } );
+    my ($button01, $button02, $button03, $button04);
+
     my $window1 =
-      Curses::Toolkit::Widget::Window->new->set_name('window')->set_title("label tests 1")
-          ->set_coordinates( x1 => 5, y1 => 5, x2 => 75, y2 => 21 );
+      Window->new->set_name('window')->set_title("Theme demonstration")
+          ->set_coordinates( x1 => 5, y1 => 5, x2 => 95, y2 => 21 );
+
     $root->add_window($window1);
     $window1->add_widget(
-        Curses::Toolkit::Widget::VBox->new()->pack_end(
-            Curses::Toolkit::Widget::Label->new->set_justify('center')->set_text("Click here to change the theme"),
-            { expand => 1, fill => 0 }
+        VBox->new()->pack_end(
+            Label->new->set_justify('center')->set_text("Click these buttons to change the theme"),
+            { expand => 1 }
         )->pack_end(
-            my $button01 =
-                Curses::Toolkit::Widget::Button->new_with_label('Change'),
-            { expand => 1, fill => 1 }
+            HBox->new()
+                ->pack_end($button01 = Button->new_with_label('Default'),   { expand => 1 } )
+                ->pack_end($button02 = Button->new_with_label('BlueWhite'), { expand => 1 } )
+                ->pack_end($button03 = Button->new_with_label('Yellow'),    { expand => 1 } )
+                ->pack_end($button04 = Button->new_with_label('Pink'),      { expand => 1 } ),
+            { expand => 0 }
         )
     );
+    $button01->signal_connect( clicked => sub { $window1->set_theme_name(Default, 1)} );
+    $button02->signal_connect( clicked => sub { $window1->set_theme_name(BlueWhite, 1)} );
+    $button03->signal_connect( clicked => sub { $window1->set_theme_name(Yellow, 1)} );
+    $button04->signal_connect( clicked => sub { $window1->set_theme_name(Pink, 1)} );
+
     POE::Kernel->run();
 }
