7319e7a5e0d7125509bc0b830b033bb11e459ff8
Damien Krotkine <dams@cpan.org>
2011-02-06 13:50:27 +0100

    updated label example

--- examples/widgets/label.pl
+++ examples/widgets/label.pl
@@ -21,10 +21,10 @@ sub main {
     my $root = POE::Component::Curses->spawn;
     my $window =
       Window->new->set_name('window')->set_title("window")
-            ->set_coordinates( x1 => 5, y1 => 5, width => 20, height => 5 );
+            ->set_coordinates( x1 => 5, y1 => 5, width => 40, height => 5 );
     $root->add_window($window);
 
-    my $label = Label->new->set_text('Hello World !');
+    my $label = Label->new->set_text('Hello World ! hit [ q ] to exit');
     $window->add_widget($label);
     POE::Kernel->run();
 }
